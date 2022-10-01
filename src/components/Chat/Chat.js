aa121dc66fd7544bdb59f0116f50f1ee94e67978
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-10-01 19:16:52 +0300

    initial-commit

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://react-chat-system-example.herokuapp.com';
+const ENDPOINT = 'https://react-chat-system-example.herokuapp.com/';
 
 let socket;
 
